bf6207168af7c0eecc5da6cfed0ed12865716a4e
John <john@minisisinc.com>
2021-06-09 14:26:12 -0700

    ADD: main content for Home;ADD: form, buttons, some minor behaviour dependency for radio btns

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+console.log('hello')
+
+const onClickSearchOption = (value) => {
+    let option = value == 'Keyword Search' ? 'Option 1: ' : 'Option 2: ';
+    let span = document.getElementById('Option-Choice');
+    span.innerText = option + value;
+}
