840be6c626cb561d259e3382cb616804889a19e0
John <john@minisisinc.com>
2021-06-14 13:02:08 -0700

    Commiting changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,27 @@ const onClickSearchOption = (value) => {
     let span = document.getElementById('Option-Choice');
     span.innerText = option + value;
 }
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+const  doSomething = (scrollPos) => {
+    // Do something with the scroll position
+    console.log(scrollPos)
+}
+
+let scroll = document.getElementById('FAQ-Wheel');
+
+scroll.addEventListener('scroll', function(e) {
+    lastKnownScrollPosition = scroll.scrollY;
+    console.log(e)
+  
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        doSomething(lastKnownScrollPosition);
+        ticking = false;
+      });
+  
+      ticking = true;
+    }
+  });
