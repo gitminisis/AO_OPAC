60c46b665f56369bd304f3389da12a55ff06e2f8
opacteam1 <nixon@minisisinc.com>
2021-06-21 16:01:43 -0700

    script cookies

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
+var HOME_SESSID = getCookie("HOME_SESSID");
+var SESSID = getCookie("SESSID");
+
 const onClickSearchOption = (value) => {
     let option = value == 'Keyword Search' ? 'Option 1: ' : 'Option 2: ';
     let span = document.getElementById('Option-Choice');
@@ -27,3 +30,39 @@ scroll.addEventListener('scroll', function(e) {
       ticking = true;
     }
   });
+
+/* * * * * * * * * * * * *
+ * *                   * *
+ * *      Cookies      * *
+ * *                   * *
+ * * * * * * * * * * * * */
+function deleteCookie(cname) {
+    document.cookie = cname + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
