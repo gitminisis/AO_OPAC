d9ba320265926a10d12b46fa1ba763a27c6763ce
john <jvbpoku@gmail.com>
2021-07-26 14:45:59 -0700

    UPDATE: create method profanity.js, renamed old profanity.js to profanityWords.js; ADD: method declarations to validate user crowd sourcing;

--- assets/js/profanityDict.js
+++ assets/js/profanityDict.js
@@ -1,3 +1,12 @@
-import profannityArr from './profanity.js'
+import { profanityArr } from './profanityWords.js';
 
-console.log(profanityArr);
+// Declare and contruct an empty set 
+export const profanityDict = new Set();
+
+// Create local copy of arr from profanity.js
+let arr = profanityArr;
+
+// Initialize values of set profanityDict
+for (let i = 0; i < arr.length; i++) 
+    profanityDict.add(arr[i]);
+
